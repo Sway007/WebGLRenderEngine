92d4635f7543f6bcc8a8805dc0863e9d15b46735
swayyan <swayyan@tencent.com>
2021-03-23 11:52:08 +0800

    feat: devops

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -13,43 +13,46 @@ const devServer = {
   host: "0.0.0.0",
 };
 
-module.exports = {
-  entry: {
-    main: path.resolve(__dirname, "react/index.tsx"),
-  },
-  output: {
-    path: path.resolve(__dirname, "output"),
-    filename: "[name].[contenthash].js",
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-  },
-  devServer,
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "babel-loader",
-      },
-      {
-        test: /\.s[ac]ss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|jpe?g)$/,
-        use: "file-loader",
-      },
+module.exports = (env, argv) => {
+  const mode = argv.mode || "development";
+  return {
+    entry: {
+      main: path.resolve(__dirname, "react/index.tsx"),
+    },
+    output: {
+      path: path.resolve(__dirname, "output"),
+      filename: "[name].[contenthash].js",
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+    devServer,
+    devtool: mode === "development" && "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: "babel-loader",
+        },
+        {
+          test: /\.s[ac]ss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.(png|jpe?g)$/,
+          use: "file-loader",
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, "index.html"),
+      }),
+      new ProgressBarPlugin(),
+      new CleanWebpackPlugin(),
+      new CleanTerminalPlugin({
+        message: `dev server running on http://localhost:${devServer.port}`,
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "index.html"),
-    }),
-    new ProgressBarPlugin(),
-    new CleanWebpackPlugin(),
-    new CleanTerminalPlugin({
-      message: `dev server running on http://localhost:${devServer.port}`,
-    }),
-  ],
+  };
 };
